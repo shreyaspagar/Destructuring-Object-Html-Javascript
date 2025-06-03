5b4a836da9fbb854fab95d6cdae1dc16a861a6d9
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-06-03 14:56:32 +0530

    Add files via upload

---
+++ app102.js
@@ -0,0 +1,35 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for (let i = 0; i < smallImages.length; i++) {
+//     smallImages[i].src = "spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed.`);
+
+// }
+
+// console.dir(document.querySelectorAll("p"));
+
+// console.dir(document.querySelector("#description"));
+
+// console.dir(document.querySelector(".oldImg"));
+
+// console.dir(document.querySelectorAll("div a"));
+
+
+
+let links = document.querySelectorAll(".box a");
+
+for (let i = 0; i < links.length; i++) {
+    links[i].style.color = "orange";
+}
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let links = document.querySelectorAll(".box a");
+//     for (let i = 0; i < links.length; i++) {
+//         links[i].style.color = "red";
+//     }
+// });
+
+
